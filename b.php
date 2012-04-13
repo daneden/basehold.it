5c0aed33eb1191942a23f5693c4ac1acdb4cc1d5
Dan Eden <dan.eden@me.com>
2012-04-13 20:12:28 +0100

    Allow baseline to scroll with content
    Nice work from @nathggns on this one!

--- b.php
+++ b.php
@@ -17,14 +17,16 @@ foreach ($_GET as $key => $value) {
 header("Content-type: text/css");
 ?>
 body:after {
-	position: fixed;
+	position: absolute;
+	widh: auto;
+	height: auto;
 	z-index: 9999;
 	content: '';
 	display: block;
 	pointer-events: none;
-	width: 100%;
-	height: 100%;
 	top: 0;
+	right: 0;
+	bottom: 0;
 	left: 0;
 	background: url(/image.php?<?php echo implode('&', $queryString); ?>) repeat top left;
 }
