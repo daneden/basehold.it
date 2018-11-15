88de2f8e4ff3f45038ebcdaa5f584487d3efaedd
Daniel Eden <dan.eden@me.com>
2018-11-15 08:02:39 -0800

    Remove stats

--- image.php
+++ image.php
@@ -10,7 +10,6 @@ $settings = array(
 	'currentHeight' => 10, 	// default to 10px height
 	'minHeight' => 1		// ensure baseline is bigger than 1px
 );
-$store = array();
 
 // Convenience function to see if a number is a valid rgb value
 function valid_rgb($val=NULL) {
@@ -19,8 +18,6 @@ function valid_rgb($val=NULL) {
 
 // Check for provided hex colour
 if(isset($_GET['hex'])){
-	// Add hex to list of things to store in stats
-	$store['hex'] = $_GET['hex'];
 	// Split hex colour into parts
 	preg_match_all("/[a-f0-9]{2}/i", $_GET['hex'], $colour);
 	// Check we have a 3 parts to the hex
@@ -37,22 +34,15 @@ if(isset($_GET['hex'])){
 
 // Check for provided R, G or B values
 }else if(
-	valid_rgb($_GET['r']) && 
-	valid_rgb($_GET['g']) && 
+	valid_rgb($_GET['r']) &&
+	valid_rgb($_GET['g']) &&
 	valid_rgb($_GET['b']))
 {
 	$colour = array($_GET['r'], $_GET['g'], $_GET['b']);
-	// Setup array of what to store in stats
-	$store = array(
-		'r' => $_GET['r'],
-		'g' => $_GET['g'],
-		'b' => $_GET['b']
-	);
 
 	// Add alpha if present
-	if (isset($_GET['a']) && $_GET['a'] >= 0 && $_GET['a'] <= 1) { 
+	if (isset($_GET['a']) && $_GET['a'] >= 0 && $_GET['a'] <= 1) {
 		$colour[] = $_GET['a'];
-		$store['a'] = $_GET['a'];
 	}
 // Use default colour if all fail
 }else{
@@ -62,25 +52,14 @@ if(isset($_GET['hex'])){
 // Default to 10 px height if they set no height.
 $settings['currentHeight'] = (isset($_GET['height']) && $_GET['height'] > $settings['minHeight']? $_GET['height'] : $settings['currentHeight']);
 
-// Store Height
-$store['height'] = $settings['currentHeight'];
-
-// Stats class include
-if(!include_once('./Stats.class.php'))
-	die('Error including Stats.class.php');
-// Instantiate instance of class
-$stats = new Stats();
-// Store the stats!
-$stats -> store($store);
-
 // Set the content-type to png
-header("Content-type: image/png"); 
+header("Content-type: image/png");
 
 // Create an image at the right dimensions.
 $im = imagecreate(4, $settings['currentHeight']);
 
 // Declare some colours
-$white = imagecolorallocate($im, 255, 255, 255);  
+$white = imagecolorallocate($im, 255, 255, 255);
 if (count($colour) == 4) {
 	$line = imagecolorallocatealpha($im, $colour[0], $colour[1], $colour[2], 127-$colour[3]*127);
 } else {
