419906e3de7c3b379dddb83152e34ba91f2506f5
Daniel Eden <dan.eden@me.com>
2018-11-15 20:04:29 -0800

    Fix formatting & change 'colour' to 'color' (sorry motherland)

--- image.php
+++ image.php
@@ -6,7 +6,7 @@
 
 // Height settings
 $settings = array(
-	'colour' => array(0, 0, 0),
+	'color' => array(0, 0, 0),
 	'currentHeight' => 10, 	// default to 10px height
 	'minHeight' => 1		// ensure baseline is bigger than 1px
 );
@@ -16,41 +16,45 @@ function valid_rgb($val=NULL) {
 	return isset($val) && $val <= 255 && $val >= 0;
 }
 
-// Check for provided hex colour
-if(isset($_GET['hex'])){
-	// Split hex colour into parts
-	preg_match_all("/[a-f0-9]{2}/i", $_GET['hex'], $colour);
+// Check for provided hex color
+if (isset($_GET['hex'])) {
+	// Split hex color into parts
+	preg_match_all("/[a-f0-9]{2}/i", $_GET['hex'], $color);
 	// Check we have a 3 parts to the hex
-	if(count($colour[0]) != 3){
-		$colour = $settings['colour'];
-	}else{
+	if (count($color[0]) != 3) {
+		$color = $settings['color'];
+	} else {
 		// Convert hex to decimal for RGB
-		$colour = array(
-			hexdec($colour[0][0]),
-			hexdec($colour[0][1]),
-			hexdec($colour[0][2])
+		$color = array(
+			hexdec($color[0][0]),
+			hexdec($color[0][1]),
+			hexdec($color[0][2])
 		);
 	}
 
 // Check for provided R, G or B values
-}else if(
+} else if (
 	valid_rgb($_GET['r']) &&
 	valid_rgb($_GET['g']) &&
 	valid_rgb($_GET['b']))
 {
-	$colour = array($_GET['r'], $_GET['g'], $_GET['b']);
+	$color = array($_GET['r'], $_GET['g'], $_GET['b']);
 
 	// Add alpha if present
 	if (isset($_GET['a']) && $_GET['a'] >= 0 && $_GET['a'] <= 1) {
-		$colour[] = $_GET['a'];
+		$color[] = $_GET['a'];
 	}
-// Use default colour if all fail
-}else{
-	$colour = $settings['colour'];
+// Use default color if all fail
+} else {
+	$color = $settings['color'];
 }
 
-// Default to 10 px height if they set no height.
-$settings['currentHeight'] = (isset($_GET['height']) && $_GET['height'] > $settings['minHeight']? $_GET['height'] : $settings['currentHeight']);
+// Default to 10px height if they set no height.
+$settings['currentHeight'] = (
+	isset($_GET['height']) && $_GET['height'] > $settings['minHeight']
+		? $_GET['height'] 
+		: $settings['currentHeight']
+);
 
 // Set the content-type to png
 header("Content-type: image/png");
@@ -58,12 +62,12 @@ header("Content-type: image/png");
 // Create an image at the right dimensions.
 $im = imagecreate(4, $settings['currentHeight']);
 
-// Declare some colours
+// Declare some colors
 $white = imagecolorallocate($im, 255, 255, 255);
-if (count($colour) == 4) {
-	$line = imagecolorallocatealpha($im, $colour[0], $colour[1], $colour[2], 127-$colour[3]*127);
+if (count($color) == 4) {
+	$line = imagecolorallocatealpha($im, $color[0], $color[1], $color[2], 127-$color[3]*127);
 } else {
-	$line = imagecolorallocatealpha($im, $colour[0], $colour[1], $colour[2], 100);
+	$line = imagecolorallocatealpha($im, $color[0], $color[1], $color[2], 100);
 }
 
 // Set the image to use white as transparent
